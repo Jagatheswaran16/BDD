29bf3fd1e8806551867ba8dd4cfbec984bef60d3
jagatheswaran.m <jagatheswaranmj@gmail.com>
2022-12-21 17:37:13 +0530

    Updated framework

--- feature/steps/base.py
+++ feature/steps/base.py
@@ -4,5 +4,6 @@ from selenium.webdriver.common.keys import Keys
 driver =webdriver.Chrome(executable_path='C:/Users/jagatheswaran.m/PycharmProjects/pythonProject/chromedriver.exe')
 driver.maximize_window()
 
+
 URL="https://rahulshettyacademy.com/seleniumPractise/#/"
 driver.implicitly_wait(5)
