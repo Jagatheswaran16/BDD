12d8720a371c4cd05ed0eecb069a03d3964c8507
jagatheswaran.m <jagatheswaranmj@gmail.com>
2023-02-21 11:25:26 +0530

    Updated framework

--- feature/steps/base.py
+++ feature/steps/base.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 driver =webdriver.Chrome(executable_path='C:/Users/jagatheswaran.m/PycharmProjects/pythonProject/chromedriver.exe')
 driver.maximize_window()
 
